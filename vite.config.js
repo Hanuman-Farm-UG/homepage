b66cf0ec53b233fbfd486834587d521a81be3dd5
surya <surya@somerandomguy.xyz>
2025-06-15 08:22:52 +0200

    enable github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace with your repo name!
+const repoName = 'homepage'
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
-  base: './'
 })
